cdd1d4522850b709ef3361aaed209272486c9b32
rahulkumarsinha <rahulkumsinha@gmail.com>
2025-10-06 00:30:11 +0530

    test modified

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -70,7 +70,7 @@ public class LoginTest extends BaseTest {
 		
 		test.info("Verifying page title...");
 		Log.info("Verifying page title...");
-		Assert.assertEquals(driver.getTitle(), "Dashboard / nopCommerce administration");
+		Assert.assertEquals(driver.getTitle(), "Dashboard / nopCommerce administration123");
 		
 		test.pass("Login Successful");
 	}
